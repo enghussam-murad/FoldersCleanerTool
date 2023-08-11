3e44777785c17c8f2b53806ed201be37762cf9d3
enghussam-murad <enghussam4045@gmail.com>
2023-08-12 00:35:45 +0300

    initial commit

---
+++ folder cleaner tool.ps1
@@ -0,0 +1,15 @@
+﻿
+$target_folder = Read-Host("Enter the directory you need to check (ex C:\Users\user\Desktop\test):")
+
+# Get all the subfolders
+$subfolders = Get-ChildItem -Path $target_folder -Recurse -Directory
+
+# Loop through each subfolder
+foreach ($subfolder in $subfolders) {
+  # Check if the subfolder is empty
+  if ((Get-ChildItem -Path $subfolder.FullName).Count -eq 0) {
+    # Delete the subfolder
+    Remove-Item -Path $subfolder.FullName -Force
+  }
+
+}
